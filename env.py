2d78418b4e7ccfc705082ad3e7479fcf0ea86345
Olivier Renouard <orenouard@gmail.com>
2007-11-19 12:27:36 +0000

    Corrected the Singleton class
    git-svn-id: https://pymel.googlecode.com/svn/trunk@13 64d93f2f-123f-0410-81d3-998474c5a783

--- env.py
+++ env.py
@@ -17,7 +17,7 @@ class Singleton(object):
 	def __new__(cls, *p, **k):
 		if not '_the_instance' in cls.__dict__:
 			cls._the_instance = object.__new__(cls)
-			return cls._the_instance
+		return cls._the_instance
 
 
 #-----------------------------------------------
