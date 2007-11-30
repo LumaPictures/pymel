6f2d00aa418f4e6424052574e6666a2786540bc0
Olivier Renouard <orenouard@gmail.com>
2007-11-30 21:43:53 +0000

    Some exemples, building the Tree representation of Maya nodes
    git-svn-id: https://pymel.googlecode.com/svn/trunk@30 64d93f2f-123f-0410-81d3-998474c5a783

---
+++ examples/dagTools.py
@@ -0,0 +1,43 @@
+"""
+    An exemple of use of the Tree library, returns the arguments, or the current selection
+    if nothing is provided :
+    Open maya file 
+    select('FBX_Hips', hierarchy=True)
+lst = ls(selection=True)
+print lst
+tree = asHierarchy (*lst)
+print tree
+print repr(tree)
+lst
+[k for k in tree]
+"""
+
+def isExactChildFn(c, p) :
+    """ a function to check if c is a direct child of p """    
+    if (c is not None) and (p is not None) :
+        #print "checking if "+c+" is child of "+p
+        prt = c.getParent()
+        if prt is not None and p is not None :
+            return prt == p
+        elif prt is None and p is None :
+            return True
+        else :
+            return False
+    else :
+        return False
+
+def asHierarchy (*args) :
+    """returns a Tree containing the PyMel objects representing Maya nodes that were passed
+        as argument, or the current seleciton if no arguments are provided,
+        in a way that mimics the Maya scene hierarchy existing on these nodes."""
+
+    if len(args) == 0 :
+        nargs = ls( selection=True)
+    else :
+        args = util.expandListArgs (*args)
+        nargs = map(PyNode, args)
+    # print "Arguments: %s"+str(nargs)
+    result = treeFromChildLink (isExactChildFn, *nargs)
+    # print "Result: %s"+str(result)
+    return result
+
