b155eb51a8c0cc31083fac76815d82491ecca5f4
Chad Dombrova <chadrik@gmail.com>
2008-01-18 03:11:02 +0000

    forgot to update runtime.py to the new faster method when i changed over the other modules
    git-svn-id: https://pymel.googlecode.com/svn/trunk@118 64d93f2f-123f-0410-81d3-998474c5a783

--- runtime.py
+++ runtime.py
@@ -7,12 +7,11 @@ import factories
 _thisModule = __import__(__name__, globals(), locals(), ['']) # last input must included for sub-modules to be imported correctly
 
 def _createFunctions():
-	for funcName, data in factories.cmdlist.items():
-		if data['type'] == 'runtime':
-			func = factories.functionFactory( funcName, None )
-			if func:
-				func.__module__ = __name__
-				setattr( _thisModule, funcName, func )
+	for funcName in factories.moduleCmds['runtime']:
+		func = factories.functionFactory( funcName, None )
+		if func:
+			func.__module__ = __name__
+			setattr( _thisModule, funcName, func )
 _createFunctions()
 
 #factories.createFunctions( _thisModule, None )
