91d63f853b77438ad93db7fed3db1cccf94bf9b9
Olivier Renouard <orenouard@gmail.com>
2008-04-06 20:16:55 +0000

    git-svn-id: https://pymel.googlecode.com/svn/trunk@365 64d93f2f-123f-0410-81d3-998474c5a783

--- util/__init__.py
+++ util/__init__.py
@@ -2,14 +2,14 @@
 "Util contains functions and classes which are required by pymel.  These helper utilities do not require pymel to operate and can be useful in other code."
 
 import sys, codecs, os, os.path, re, platform
-from exceptions import *
+
 from collections import *
 
+from mexceptions import *
 from namedtuple import namedtuple
 from common import *
 from arguments import *
 from utilitytypes import *
-from nameparse import *
 
 import envparse
 
