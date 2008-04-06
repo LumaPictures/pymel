f39c85dffa7759463c836856f0a0e0cfba2cc0c5
Olivier Renouard <orenouard@gmail.com>
2008-04-06 20:02:29 +0000

    __init__.py for external utilities
    git-svn-id: https://pymel.googlecode.com/svn/trunk@361 64d93f2f-123f-0410-81d3-998474c5a783

---
+++ util/external/__init__.py
@@ -0,0 +1,5 @@
+""" External utilities that are required by pymel """
+
+# TODO : version check in case user has a more recent version in site-packages and we get compatibility problems
+import ply.lex as lex
+import ply.yacc as yacc
