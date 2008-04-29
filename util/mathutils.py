01346f5206408923c0c55b4782b716fd7e17ccdb
Olivier Renouard <orenouard@gmail.com>
2008-04-29 10:20:00 +0000

    moved to utils
    git-svn-id: https://pymel.googlecode.com/svn/trunk@423 64d93f2f-123f-0410-81d3-998474c5a783

---
+++ util/mathutils.py
@@ -0,0 +1,259 @@
+""" Imports Python math module and adds some additionnal math related utilities. """
+
+# TODO : What about using Numpy ?
+
+import math
+
+def dot(a, b):
+    """ dot product of two Vectors """
+    return a.x*b.x+a.y*b.y+a.z*b.z
+
+def cross(a, b):
+    """ cross product of two Vectors """
+    return Vector(a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x)
+
+# NOTE: a, b, c are points
+def cotan(a, b, c) :
+    """ cotangent of the (b-a), (c-a) angle """
+    return (((c - b)*(a - b))/((c - b)^(a - b)).length());
+
+# elementwise operations
+    
+def log10(a):
+    log10.__doc__
+    try:
+        return a.__class__(map(math.log10, a))
+    except:
+        return math.log10(a)
+
+def log(a):
+    log.__doc__
+    try:
+        return a.__class__(map(math.log, a))
+    except:
+        return math.log(a)
+        
+def exp(a):
+    exp.__doc__
+    try:
+        return a.__class__(map(math.exp, a))
+    except:
+        return math.exp(a)
+
+def sin(a):
+    sin.__doc__
+    try:
+        return a.__class__(map(math.sin, a))
+    except:
+        return math.sin(a)
+        
+def tan(a):
+    tan.__doc__
+    try:
+        return a.__class__(map(math.tan, a))
+    except:
+        return math.tan(a)
+        
+def cos(a):
+    cos.__doc__
+    try:
+        return a.__class__(map(math.cos, a))
+    except:
+        return math.cos(a)
+
+def asin(a):
+    asin.__doc__
+    try:
+        return a.__class__(map(math.asin, a))
+    except:
+        return math.asin(a)
+
+def atan(a):
+    atan.__doc__
+    try:
+        return a.__class__(map(math.atan, a))
+    except:
+        return math.atan(a)
+
+def acos(a):
+    acos.__doc__
+    try:
+        return a.__class__(map(math.acos, a))
+    except:
+        return math.acos(a)
+
+def sqrt(a):
+    sqrt.__doc__
+    try:
+        return a.__class__(map(math.sqrt, a))
+    except:
+        return math.sqrt(a)
+
+def sinh(a):
+    sinh.__doc__
+    try:
+        return a.__class__(map(math.sinh, a))
+    except:
+        return math.sinh(a)
+
+def tanh(a):
+    tanh.__doc__
+    try:
+        return a.__class__(map(math.tanh, a))
+    except:
+        return math.tanh(a)
+
+def cosh(a):
+    cosh.__doc__
+    try:
+        return a.__class__(map(math.cosh, a))
+    except:
+        return math.cosh(a)
+
+
+def pow(a,b):
+    """
+    Takes the elements of a and raises them to the b-th power
+    """
+    try:
+        return a.__class__(map(lambda x: x**b, a))
+    except:
+        try:
+            return a.__class__(map(lambda x,y: x**y, a, b))
+        except:
+            return math.pow(a,b)
+    
+def atan2(a,b):       
+    """
+    Arc tangent
+    
+    """
+    try:
+        return a.__class__(map(atan2, a, b))
+    except:
+        return math.atan2(a,b)
+    
+
+# general remapping operations
+
+def gamma (c, g):
+    """ gamma color correction with a single scalar gamma value g"""
+    try:
+        return c.__class__(map(lambda x:x**g, c))
+    except:
+        return c**g 
+
+# TODO : modify these so that they accept iterable / element wise operations
+
+def smoothmap(min, max, x):
+    """Returns the value of a smooth remapping function.
+
+    performs a smooth Hermite interpolation between 0 and 1 in the interval min to max,
+    but does not clamp the range
+    """
+    x = float(x)
+    x = float(x-min)/float(max-min)
+    return x*x*(3.0-2.0*x)
+    
+def smoothstep(min, max, x):
+    """Returns the value of a smooth step function.
+
+    Returns 0 if x < min, 1 if x > max, and performs a smooth Hermite
+    interpolation between 0 and 1 in the interval min to max.
+    """
+    if x<min:
+        return 0.0
+    if x>max:
+        return 1.0
+    return smoothmap(min, max, x)
+
+def linmap(min, max, x):
+    """Returns the value of a linear remapping function.
+
+    performs a linear interpolation between 0 and 1 in the interval min to max,
+    but does not clamp the range
+    """
+    return (float(x)-min)/(max-min)
+    
+def linstep(min, max, x):
+    """Returns the value of a linear step function.
+
+    Returns 0 if x < min, 1 if x > max, and performs a linear
+    interpolation between 0 and 1 in the interval min to max.
+    """
+    if x<min:
+        return 0.0
+    if x>max:
+        return 1.0
+    return linmap(min, max, x)
+
+# NOTE : x first seem more natural    
+def clamp(x=0.0, min=0.0, max=1.0) :
+    """ Clamps the value x between min and max """
+    # NOTE : in 2.5 can use 'caseTrue if condition else caseFalse'
+    #realmin = min if min<max else max
+    #realmax = max if min<max else min
+    # orig C code :
+    #    const double realmin=(min<max)?min:max;
+    #    const double realmax=(min<max)?max:min;
+    #    return ((x<realmin)?realmin:(x>realmax)?realmax:x);
+    if min<max :
+        realmin = min
+        realmax = max
+    else :
+        realmin = max
+        realmax = min
+    if x<realmin :
+        result = realmin
+    elif x>realmax :
+        result = realmax
+    else :
+        result = x    
+    return result
+
+def setRange(x=0.0, oldmin=0.0, oldmax=1.0, newmin=0.0, newmax=1.0) :
+    """ Resets x range from x linear interpolation of oldmin to oldmax to x linear interpolation from newmin to newmax """
+    if oldmin<oldmax :
+        realoldmin=oldmin
+        realoldmax=oldmax
+        realnewmin=newmin
+        realnewmax=newmax
+    elif oldmin>oldmax :
+        realoldmin=oldmax
+        realoldmax=oldmin
+        realnewmin=newmax
+        realnewmax=newmin
+    else :
+        return x
+    if x<realoldmin :
+        result = realnewmin
+    elif x>realoldmax :
+        result = realnewmax
+    else :
+        result = (realnewmin+(realnewmax-realnewmin)*(x-oldmin)/(oldmax-oldmin))  
+    return result
+
+def hermiteInterp(x=0.0, y0=0.0, y1=1.0, s0=0.0, s1=0.0) :
+    """ Hermite interpolation of x between points y0 and y1 of tangent slope s0 and s1 """
+    c = s0
+    v = y0 - y1
+    w = c + v
+    x = w + v + s1
+    b_neg = w + x
+    return ((((x * x) - b_neg) * x + s0) * x + y0)
+
+def hermite(x=0.0, v0=0.0, v1=0.0, s0=0.0, s1=0.0) :
+    """ As the MEL command : This command returns x point along on x hermite curve from the five given control arguments.
+        The first two arguments are the start and end points of the curve, respectively.
+        The next two arguments are the tangents of the curve at the start point and end point of the curve, respectively.
+        The fifth argument, parameter, specifies the point on the hermite curve that is returned by this function.
+        This parameter is the unitized distance along the curve from the start point to the end point.
+        A parameter value of 0.0 corresponds to the start point and x parameter value of 1.0 corresponds to the end point of the curve. """
+
+    if x<0.0 :
+        res = v0
+    elif x>1.0 :
+        res = v2
+    else :
+        res = hermiteInterp(x, v0, v1, s0, s1)
+    return res    
