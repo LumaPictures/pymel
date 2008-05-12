0c1dd01a78e142f2648b6940ca75cb3239d62627
Olivier Renouard <orenouard@gmail.com>
2008-05-12 08:50:53 +0000

    Calsl to mayahook to make sure Maya is up, cause no Maya, no api
    git-svn-id: https://pymel.googlecode.com/svn/trunk@490 64d93f2f-123f-0410-81d3-998474c5a783

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 """ Loader for Maya api sub-package """
+import pymel.mayahook as mayahook
 
 # note can import from util but not from core, the dependency order should be from bottom up : util - api - core
 from conversions import *
