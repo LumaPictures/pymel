2a1c8129457303026bb46cbe4176349d69ec09d2
Chad Dombrova <chadrik@gmail.com>
2008-06-28 07:28:04 +0000

    MFn work.  updated ApiDocParser, and added a few more castings to wrapApiMethod
    git-svn-id: https://pymel.googlecode.com/svn/trunk@592 64d93f2f-123f-0410-81d3-998474c5a783

--- core/maths.py
+++ core/maths.py
@@ -1,5 +1,4 @@
 
 from math import *
 from pymel.util.mathutils import *
-from pymel.util.arrays import *
-from pmtypes.mayatypes import * 
+#from pymel.util.arrays import *  # conflicts with wrappedtypes.Vector
