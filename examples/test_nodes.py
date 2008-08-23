ba1fc400ab5a24e785a6a87d78aa1ef5ccfeb486
Chad Dombrova <chadrik@gmail.com>
2008-08-23 03:42:09 +0000

    started creating some unittests
    git-svn-id: https://pymel.googlecode.com/svn/trunk@721 64d93f2f-123f-0410-81d3-998474c5a783

--- examples/test_nodes.py
+++ examples/test_nodes.py
@@ -1,5 +1,5 @@
 import sys, os, inspect, unittest
-
+from pymel import *
 #import pymel
 #import pymel.core.factories as _factories
 #import maya.cmds as cmds
@@ -20,20 +20,51 @@ import sys, os, inspect, unittest
 #    #print emptyFunctions
 
 
-class testCase_attributes(unittest.TestCase):
+class testCase_nodesAndAttributes(unittest.TestCase):
 
     def setUp(self):
         self.sphere1, hist = polySphere()
         self.cube1, hist = polyCube()
         self.grp1 = group(self.sphere1, self.cube1)
+        
+        #duplicate
         self.grp2 = duplicate(self.grp1)[0]
         self.sphere2, self.cube2 = self.grp2.getChildren()
         
-    def test01_isDagOnObjects(self):
-        self.sphere2.t.tx.parent()
+        #instance
+        self.grp3 = instance(self.grp1)[0]
+        self.sphere3, self.cube3 = self.grp3.getChildren()
+        
+    def test01_attribute_parent_equality(self):
         self.assertEqual( self.sphere2.t.tx.parent(), self.sphere2.t )
+        
+    def test02_attribute_duplicate_inequality(self):
+        self.assert_( self.sphere1.t != self.sphere2.t )
+        
+    def test03_attribute_instance_equality(self):
+        self.assertEqual( self.sphere1.t, self.sphere3.t )
+    
+    def test04_attribute_cascading(self):
+        self.sphere1.primaryVisibility.set(1)
+        shape = self.sphere1.getShape()
+        self.assertEqual( self.sphere1.primaryVisibility, shape.primaryVisibility )
+    
+    def test05_dagNode_addParent(self):
+        sphere = polySphere()[0]
+        cube = polyCube()[0]
+        torus = polyTorus()[0]
+        sphere | cube | torus
+        print torus.fullpath()
+        
+    #def test05_dagNode_getParent(self):
+    def test_instances(self):
+        self.assert_( self.sphere1.isInstance( self.sphere3) )   
         
     def tearDown(self):
         # cleaning
-        delete(self.grp1,self.grp2)
-        
+        delete(self.grp1,self.grp2, self.grp3)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(testCase_nodesAndAttributes)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+     
