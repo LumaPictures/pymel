7101a37a86c3c02877601143e4ec42217f66c15a
Olivier Renouard <orenouard@gmail.com>
2008-08-27 19:17:44 +0000

    small fix to warnings
    git-svn-id: https://pymel.googlecode.com/svn/trunk@751 64d93f2f-123f-0410-81d3-998474c5a783

--- util/pwarnings.py
+++ util/pwarnings.py
@@ -1,9 +1,16 @@
 """
     Redefine format warning to avoid getting garbage at end of line when raised directly from Maya console
     and define a UserWarning class that does only print it's message (no line or module info)
+    
+    >>> import sys
+    >>> sys.stderr = sys.stdout
+    >>> warn("An ExecutionWarning", ExecutionWarning) #doctest: +ELLIPSIS
+    /...ExecutionWarning: An ExecutionWarning...
+    
 """
+
 import os.path
-from warnings import *
+from warnings import defaultaction, filters, filterwarnings, formatwarning, linecache, onceregistry, resetwarnings, showwarning, simplefilter, warn, warn_explicit
 
 def formatwarning(message, category, filename, lineno):
     """Redefined format warning for maya."""
@@ -14,7 +21,7 @@ def formatwarning(message, category, filename, lineno):
         name, ext = os.path.splitext(filename)
         line = None
         if ext == ".py" :
-            line = unicode(warnings.linecache.getline(filename, lineno)).strip()
+            line = unicode(linecache.getline(filename, lineno)).strip()
         if line and len(line) > 0 :
             s = s + " " + line + "\n"
     return s
@@ -22,4 +29,7 @@ def formatwarning(message, category, filename, lineno):
 
 class ExecutionWarning (UserWarning) :
     """ Simple Warning class that doesn't print any information besides warning message """
-    
+  
+if __name__ == '__main__' :
+    import doctest
+    doctest.testmod()   
