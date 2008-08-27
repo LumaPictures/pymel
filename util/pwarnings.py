18ccfa6928c4bdea8e7e5454ffc8ad94b68a1b5c
Olivier Renouard <orenouard@gmail.com>
2008-08-27 22:12:26 +0000

    fix formatwarning
    git-svn-id: https://pymel.googlecode.com/svn/trunk@755 64d93f2f-123f-0410-81d3-998474c5a783

--- util/pwarnings.py
+++ util/pwarnings.py
@@ -3,29 +3,38 @@
     and define a UserWarning class that does only print it's message (no line or module info)
     
     >>> import sys
-    >>> sys.stderr = sys.stdout
+    >>> sys.stderr = sys.stdout   
     >>> warn("An ExecutionWarning", ExecutionWarning) #doctest: +ELLIPSIS
     /...ExecutionWarning: An ExecutionWarning...
     
 """
 
 import os.path
-from warnings import defaultaction, filters, filterwarnings, formatwarning, linecache, onceregistry, resetwarnings, showwarning, simplefilter, warn, warn_explicit
+import warnings
+from warnings import formatwarning, linecache, resetwarnings, simplefilter, warn
+# from warnings import simplefilter, warn
 
 def formatwarning(message, category, filename, lineno):
     """Redefined format warning for maya."""
     if issubclass(category, ExecutionWarning) :
-        s =  unicode("%s: %s\n") % (category.__name__, message)
+        s =  u"%s: %s\n" % (category.__name__, message)
     else :
-        s =  unicode("%s: %s\n at line: %s in %s\n") % (category.__name__, message, lineno, filename)
+        # print "category is", category.__name__
+        # print "message is ", message
+        # print "lineno is ", lineno
+        # print "filename is ", filename
+        s =  u"%s: %s at line: %s in %s\n" % (category.__name__, message, lineno, filename)
         name, ext = os.path.splitext(filename)
-        line = None
+        line = ""
         if ext == ".py" :
             line = unicode(linecache.getline(filename, lineno)).strip()
-        if line and len(line) > 0 :
-            s = s + " " + line + "\n"
+        if len(line) > 0 :
+            # print "adding line %s" % line
+            s = s + u"#\t" + line + u"\n"
+        # print "s:\n", s
     return s
 
+warnings.formatwarning = formatwarning
 
 class ExecutionWarning (UserWarning) :
     """ Simple Warning class that doesn't print any information besides warning message """
