682dae9f65b84443885e70b13e0e9e2341588b6b
Chad Dombrova <chadrik@gmail.com>
2008-09-06 03:26:05 +0000

    wrapped pointPosition to return MVector
    git-svn-id: https://pymel.googlecode.com/svn/trunk@775 64d93f2f-123f-0410-81d3-998474c5a783

--- core/modeling.py
+++ core/modeling.py
@@ -1,4 +1,9 @@
 
 import pmtypes.factories as _factories
 import general
+import pmtypes.pmcmds as cmds
+
+def pointPosition( *args, **kwargs ):
+    return general.MPoint( cmds.pointPosition(*args, **kwargs) )
+
 _factories.createFunctions( __name__, general.PyNode )
