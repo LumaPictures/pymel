2128ace7764c5d444257d862b96886506754b04e
Chad Dombrova <chadrik@gmail.com>
2008-12-01 18:23:24 +0000

    added mel errors and line numbers to python tracebacks when calling mel via Mel class.  added special Mel exception classes. made Mel methods static.
    git-svn-id: https://pymel.googlecode.com/svn/trunk@929 64d93f2f-123f-0410-81d3-998474c5a783

--- core/language.py
+++ core/language.py
@@ -10,6 +10,9 @@ import pmtypes.pmcmds as cmds
 from pymel.mayahook.optionvars import *
 import os
 import pmtypes.factories as _factories
+import pymel.api as api
+import pmtypes.wrappedtypes as _types
+
 #--------------------------
 # Mel <---> Python Glue
 #--------------------------  
@@ -222,12 +225,28 @@ catch = Catch()
 #--------------------------
 # Maya.mel Wrapper
 #--------------------------
-    
+
+class MelError(RuntimeError):
+    """Generic MEL error"""
+    pass
+
+class MelConversionError(MelError,TypeError):
+    """Error raised when MEL cannot process a conversion or cast between data types"""
+    pass
+
+class UnknownMelProcedure(MelError,NameError):
+    """The called mel procedure does not exist or has not been sourced"""
+    pass
+
+class MelArgumentError(MelError,TypeError):
+    """The arguments passed to the mel script are incorrect"""
+    pass
+
 class Mel(object):
     """This class is a convenience for calling mel scripts from python, but if you are like me, you'll quickly find that it
     is a necessity. It allows mel scripts to be called as if they were python functions: it automatically formats python
-    arguments into a command string which is executed via maya.mel.eval().  An instance of this class is already created for you 
-    when importing pymel and is called mel.  
+    arguments into a command string which is executed via maya.mel.eval.  An instance of this class is already created for you 
+    when importing pymel and is called `mel`.  
     
     
         
@@ -261,6 +280,27 @@ class Mel(object):
     
     In this you can see how `pymel.core.mel` allows you to pass any python object directly to your mel script as if 
     it were a python function, with no need for formatting arguments.  The resulting code is much more readable.
+    
+    Another advantage of this class over maya.mel.eval is its handling of mel errors.  If a mel procedure fails to
+    execute, you will get the specific mel error message in the python traceback, and, if they are enabled, line numbers!
+    
+    For example, in the example below we redeclare the myScript procedure with a line that will result in an error:
+    
+        >>> mel.eval( '''global proc myScript( string $stringArg, float $floatArray[] ){ 
+        ...     float $donuts = `ls -type camera`;}''')
+        >>> mel.myScript( 'foo', [] )
+        Error: line 2: Cannot convert data of type string[] to type float.
+        Traceback (most recent call last):
+            ...
+        pymel.core.language.MelConversionError: Error occurred during execution of MEL script: line 2: Cannot convert data of type string[] to type float.
+
+    
+    Notice that the error raised is a `MelConversionError`.  There are several MEL exceptions that may be raised,
+    depending on the type of error: `MelError`, `MelConversionError`, `MelArgumentError`, and `UnknownProcedureError`.
+    To remain backward compatible with maya.cmds and older versions of pymel, all MEL exceptions inherit from 
+    `MelError`, which in turn inherits from `RuntimeError`.
+    
+    
     """
             
     def __getattr__(self, command):
@@ -276,37 +316,29 @@ class Mel(object):
                 
             else:
                 cmd = '%s(%s)' % ( command, ','.join( strArgs ) )
-            #print cmd
-            try:
-                return mm.eval(cmd)
-            except RuntimeError, msg:
-                info = self.whatIs( command )
-                if info.startswith( 'Presumed Mel procedure'):
-                    raise NameError, 'Unknown Mel procedure'
-                raise RuntimeError, msg
             
+            return self.eval(cmd)
+            #print cmd
+#            try:
+#                return mm.eval(cmd)
+#            except RuntimeError, msg:
+#                info = self.whatIs( command )
+#                if info.startswith( 'Presumed Mel procedure'):
+#                    raise NameError, 'Unknown Mel procedure'
+#                raise RuntimeError, "%s: %s" % ( msg, cmd )
+
         return _call
     
-    def call(self, command, *args ):
-        strArgs = map( pythonToMel, args)
-                        
-        cmd = '%s(%s)' % ( command, ','.join( strArgs ) )
 
-        try:
-            return mm.eval(cmd)
-        except RuntimeError, msg:
-            info = self.whatIs( command )
-            if info.startswith( 'Presumed Mel procedure'):
-                raise NameError, 'Unknown Mel procedure'
-            raise RuntimeError, msg
-    
+    @staticmethod
     def mprint(self, *args):
         """mel print command in case the python print command doesn't cut it. i have noticed that python print does not appear
         in certain output, such as the rush render-queue manager."""
         #print r"""print (%s\\n);""" % pythonToMel( ' '.join( map( str, args))) 
         mm.eval( r"""print (%s);""" % pythonToMel( ' '.join( map( str, args))) + '\n' )
-        
-    def source( self, script, language='mel' ):
+    
+    @staticmethod
+    def source( script, language='mel' ):
         """use this to source mel or python scripts.
         language : 'mel', 'python'
             When set to 'python', the source command will look for the python equivalent of this mel file, if
@@ -330,33 +362,111 @@ class Mel(object):
             
         else:
             raise TypeError, "language keyword expects 'mel' or 'python'. got '%s'" % language
-            
-    def eval( self, command ):
-        # should return a value, like mm.eval
-        return mm.eval( command )    
     
-    def error( self, msg, showLineNumber=False ):       
+    @staticmethod        
+    def eval( cmd ):
+        # should return a value, like mm.eval
+        #return mm.eval( cmd )    
+        global errors
+        errors = []
+        def errorCallback( nativeMsg, messageType, data ):
+            global errors
+            if messageType == api.MCommandMessage.kError:
+                if nativeMsg:
+                    errors +=  [ nativeMsg ]
+        
+        # setup the callback
+        id = api.MCommandMessage.addCommandOutputCallback( errorCallback, None )
+        
+        
+        try:
+            res = api.MCommandResult()
+            api.MGlobal.executeCommand( cmd, res, True, True )
+        except:
+            api.MMessage.removeCallback( id )
+            msg = '\n'.join( errors)
+            if 'Cannot find procedure' in msg:
+                e = UnknownMelProcedure
+            elif 'Wrong number of arguments' in msg:
+                e = MelArgumentError
+            elif 'Cannot convert data' in msg or 'Cannot cast data' in msg:
+                e = MelConversionError
+            else:
+                e = MelError
+            raise e, "Error occurred during execution of MEL script: %s" % ( msg )
+        else:   
+            api.MMessage.removeCallback( id )
+            resType = res.resultType()
+            
+            if resType == api.MCommandResult.kInvalid:
+                return   
+            elif resType == api.MCommandResult.kInt:
+                result = api.MScriptUtil().asIntPtr()
+                res.getResult(result)
+                api.MScriptUtil(result).asInt()
+                return result  
+            elif resType == api.MCommandResult.kIntArray:
+                result = api.MIntArray()
+                res.getResult(result)
+                return [ result[i] for i in range( result.length() ) ]
+            elif resType == api.MCommandResult.kDouble:
+                result = api.MScriptUtil().asDoublePtr()
+                res.getResult(result)
+                api.MScriptUtil(result).asDouble()
+                return result  
+            elif resType == api.MCommandResult.kDoubleArray:
+                result = api.MDoubleArray()
+                res.getResult(result)
+                return [ result[i] for i in range( result.length() ) ]
+            elif resType == api.MCommandResult.kString:
+                return res.stringResult()
+            elif resType == api.MCommandResult.kStringArray:
+                result = []
+                res.getResult(result)
+                return result
+            elif resType == api.MCommandResult.kVector:
+                result = api.MVector()
+                res.getResult(result)
+                return _types.Vector(result)    
+            elif resType == api.MCommandResult.kVectorArray:
+                result = api.MMatrixArray()
+                res.getResult(result)
+                return [ _types.Vector(result[i]) for i in range( result.length() ) ]
+            elif resType == api.MCommandResult.kMatrix:
+                result = api.MMatrix()
+                res.getResult(result)
+                return _types.Matrix(result)   
+            elif resType == api.MCommandResult.kMatrixArray:
+                result = api.MMatrixArray()
+                res.getResult(result)
+                return [ _types.Matrix(result[i]) for i in range( result.length() ) ]
+             
+    @staticmethod   
+    def error( msg, showLineNumber=False ):       
         if showLineNumber:
             flags = ' -showLineNumber true '
         else:
             flags = ''
         mm.eval( """error %s %s""" % ( flags, pythonToMel( msg) ) )
 
-    def warning( self, msg, showLineNumber=False ):       
+    @staticmethod
+    def warning( msg, showLineNumber=False ):       
         if showLineNumber:
             flags = ' -showLineNumber true '
         else:
             flags = ''
         mm.eval( """warning %s %s""" % ( flags, pythonToMel( msg) ) )
 
-    def trace( self, msg, showLineNumber=False ):       
+    @staticmethod
+    def trace( msg, showLineNumber=False ):       
         if showLineNumber:
             flags = ' -showLineNumber true '
         else:
             flags = ''
         mm.eval( """trace %s %s""" % ( flags, pythonToMel( msg) ) )
-                  
-    def tokenize(self, *args ):
+    
+    @staticmethod
+    def tokenize( *args ):
         raise NotImplementedError, "Calling the mel command 'tokenize' from python will crash Maya. Use the string split method instead."
 
 mel = Mel()
