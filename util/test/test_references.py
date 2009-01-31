482315927d922c872fd78e20f4b0c68167eeb0d5
Chad Dombrova <chadrik@gmail.com>
2009-01-31 03:44:56 +0000

    beginning of a unittest for references
    git-svn-id: https://pymel.googlecode.com/svn/trunk@1123 64d93f2f-123f-0410-81d3-998474c5a783

---
+++ util/test/test_references.py
@@ -0,0 +1,25 @@
+
+
+class testCase_references(unittest.TestCase):
+
+    def setUp(self):
+        # create sphere file
+        newFile(f=1)
+        polySphere()
+        self.sphereFile = saveAs( os.path.join( temp, 'sphere.ma' ), f=1 )
+        
+        # create sphere file
+        newFile(f=1)
+        polyCube()
+        createReference( sphereFile, namespace='sphere' )
+        self.cubeFile = saveAs( os.path.join( temp, 'cube.ma' ), f=1 )
+        
+        newFile(f=1)
+        self.sphereRef1 = createReference( sphereFile, namespace='sphere1' )
+        self.sphereRef2 = createReference( sphereFile, namespace='sphere2' )
+        self.cubeRef1 = createReference( cubeFile, namespace='cube1' )
+        
+    
+    
+    def tearDown(self):
+        newFile(f=1)
