1064a80a1261de318144d244f237b3d8e37be826
Chad Dombrova <chadrik@gmail.com>
2009-04-22 23:33:44 -0700

    removed input prompt for installing ipython dependency

--- setup.py
+++ setup.py
@@ -5,31 +5,6 @@ ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-def getInput():
-    res = raw_input( '> install ipymel, customized ipython interpreter (y/N) [n]:' )
-    if res.lower() in ['n', 'no']:
-        return False
-    elif res.lower() in ['y', 'yes']:
-        return True
-    else:
-        print "* Please enter either 'y' or 'n'."
-        return getInput()
-
-requires = ['BeautifulSoup >3.0', 'ply >2.0, <3.0' ]
-entry_points = {}
-install_ipymel = False
-try:
-    import IPython
-    install_ipymel = True
-except ImportError:
-    if getInput():
-        install_ipymel = True
-        requires.append( 'ipython' )
-
-if install_ipymel:
-    entry_points['console_scripts'] = ['ipymel = pymel.tools.ipymel:main [ipymel]']
-
-
 setup(name='pymel',
       version='0.9.1',
       description='Python in Maya Done Right',
@@ -43,12 +18,9 @@ succinct and intuitive way. """,
       author_email='chadrik@gmail.com',
       url='http://code.google.com/p/pymel/',
       packages=['pymel','pymel.api', 'pymel.core', 'pymel.mayahook', 'pymel.tools', 'pymel.tools.mel2py', 'pymel.util' ],
-      #package_dir={'':'pymel'},
-      #packages=find_packages('pymel'),
-      #scripts=['scripts/ipymel'],
-      entry_points = entry_points,
+      entry_points = {'console_scripts' : 'ipymel = pymel.tools.ipymel:main [ipymel]' },
       package_data={'pymel': ['*.bin', '*.conf' ] },
-      install_requires=requires,
+      install_requires=['BeautifulSoup >3.0', 'ply >2.0, <3.0' ],
       extras_require= { 'ipymel' : 'ipython' },
       tests_require=['nose'],
       test_suite = 'nose.collector',
