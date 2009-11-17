6dd8ed9c4e0ea8753697ee86c6fcc690254e2090
Paul Molodowitch <elrond79@gmail.com>
2009-11-16 19:41:57 -0800

    added mayaBug test - surfaceRange domain

--- tests/test_mayaBugs.py
+++ tests/test_mayaBugs.py
@@ -3,7 +3,8 @@ import maya.cmds as cmds
 import maya.OpenMaya as om
 import maya.OpenMayaFX as fx
 
-class testCase_fluidMFn(unittest.TestCase):
+# Bug report 344037
+class TestFluidMFnCreation(unittest.TestCase):
     def setUp(self):
         cmds.file(new=1, f=1)
         
@@ -14,3 +15,70 @@ class testCase_fluidMFn(unittest.TestCase):
         dag = om.MDagPath()
         selList.getDagPath(0, dag)
         fx.MFnFluid(dag)
+        
+# Bug report 
+class TestSurfaceRangeDomain(unittest.TestCase):
+    def setUp(self):
+        cmds.file(new=1, f=1)
+        
+    def runTest(self):
+        # create a nurbs sphere
+        mySphere = cmds.sphere()[0]
+        # a default sphere should have u/v
+        # parameter ranges of 0:4/0:8
+        
+        # The following selections should
+        # result in one of these:
+        desiredResults = ('nurbsSphere1.u[2:3][0:8]',
+                          'nurbsSphere1.u[2:3][*]',
+                          'nurbsSphere1.u[2:3]',
+                          'nurbsSphere1.uv[2:3][0:8]',
+                          'nurbsSphere1.uv[2:3][*]',
+                          'nurbsSphere1.uv[2:3]',
+                          'nurbsSphere1.v[0:8][2:3]',
+                          'nurbsSphere1.v[*][2:3]')
+
+                
+        # Passes
+        cmds.select('nurbsSphere1.u[2:3][*]')
+        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+        
+        # Passes
+        cmds.select('nurbsSphere1.v[*][2:3]')
+        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)        
+
+        # Fails! - returns 'nurbsSphere1.u[2:3][0:1]'
+        cmds.select('nurbsSphere1.u[2:3]')
+        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+        
+        # Fails! - returns 'nurbsSphere1.u[2:3][0:1]'
+        cmds.select('nurbsSphere1.uv[2:3][*]')
+        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+
+        # The following selections should
+        # result in one of these:
+        desiredResults = ('nurbsSphere1.u[0:4][2:3]',
+                          'nurbsSphere1.u[*][2:3]',
+                          'nurbsSphere1.uv[0:4][2:3]',
+                          'nurbsSphere1.uv[*][2:3]',
+                          'nurbsSphere1.v[2:3][0:4]',
+                          'nurbsSphere1.v[2:3][*]')
+                          'nurbsSphere1.v[2:3]')
+                
+        # Passes
+        cmds.select('nurbsSphere1.u[*][2:3]')
+        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+
+        # Passes
+        cmds.select('nurbsSphere1.v[2:3][*]')
+        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+
+        # Fails! - returns 'nurbsSphereShape1.u[0:1][2:3]'
+        cmds.select('nurbsSphere1.v[2:3]')
+        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+
+        # Fails! - returns 'nurbsSphereShape1.u[0:4][0:1]'
+        cmds.select('nurbsSphere1.uv[*][2:3]')
+        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+
+        
