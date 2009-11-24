4493027ae3548fa419a4746bb4f1f250a10aec14
Chad Dombrova <chadd@luma-pictures.com>
2009-11-24 15:02:29 -0800

    core.modeling: ensure that the 'name' flag for surface and curve operates on shape as well

--- pymel/core/modeling.py
+++ pymel/core/modeling.py
@@ -7,4 +7,28 @@ import general as _general
 def pointPosition( *args, **kwargs ):
     return _general.datatypes.Point( cmds.pointPosition(*args, **kwargs) )
 
+def curve( *args, **kwargs ):
+    """
+Maya Bug Fix:
+  - name paramater only applied to transform. now applies to shape as well
+    """
+    # curve returns a transform
+    name = kwargs.pop('name', kwargs.pop('n', None))
+    res = _general.PyNode( cmds.curve(*args, **kwargs) )
+    if name:
+        res.rename(name)
+    return res
+
+def surface( *args, **kwargs ):
+    """
+Maya Bug Fix:
+  - name paramater only applied to transform. now applies to shape as well
+    """
+    # surface returns a shape
+    name = kwargs.pop('name', kwargs.pop('n', None))
+    res = _general.PyNode( cmds.surface(*args, **kwargs) )
+    if name:
+        res.getParent().rename(name)
+    return res
+
 _factories.createFunctions( __name__, _general.PyNode )
