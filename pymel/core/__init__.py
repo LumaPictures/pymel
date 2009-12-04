63eb7f0338debfd9f4ccaba937499bb7043f30ae
Ian Jones <ian@ambientdivide.com>
2009-12-03 19:02:19 -0800

    Reorder init now that bug is fixed

--- pymel/core/__init__.py
+++ pymel/core/__init__.py
@@ -17,13 +17,13 @@ import pymel.api as _api
 from general import *
 from context import *
 from system import *
+from windows import *
 from animation import *
 from effects import *
 from modeling import *
 from rendering import *
 from language import *
 from other import *
-from windows import *
 
 # to allow lazy loading, we avoid import *
 import nodetypes
