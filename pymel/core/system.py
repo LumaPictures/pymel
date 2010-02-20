2f3ae313b2432e1e736b7e2eebb2a2957b967241
Chad Dombrova <chadrik@gmail.com>
2010-02-20 12:35:14 -0800

    fixed some keywords that had been mistakenly refactored.  used internal.startup instead of internal

--- pymel/core/system.py
+++ pymel/core/system.py
@@ -47,8 +47,9 @@ _logger = _internal.getLogger(__name__)
 
 
 try:
+    from pymel.internal.startup import pymel_options as _pymel_options
     # attempt to import a custom path class to use as the base for pymel's Path class
-    basePathName = _internal.pymel_options['path_class']
+    basePathName = _pymel_options['path_class']
     buf = basePathName.split('.')
     moduleName = '.'.join(buf[:-1])
     className = buf[-1]
@@ -178,14 +179,14 @@ class Namespace(str):
             raise Exception("Node '%s' does not exist" % node)
         return node
 
-    def listNamespaces(self, recursive=False, _internal=False):
+    def listNamespaces(self, recursive=False, internal=False):
         curNS = Namespace.getCurrent()
 
         self.setCurrent()
         try:
             namespaces = map(self.__class__, cmds.namespaceInfo(listOnlyNamespaces=True) or [])
 
-            if not _internal:
+            if not internal:
                 for i in [":UI",":shared"]:
                     if i in namespaces:
                         namespaces.remove(i)
@@ -194,7 +195,7 @@ class Namespace(str):
                 childNamespaces = []
                 for ns in namespaces:
                     try:
-                        childNamespaces.extend(ns.listNamespaces(recursive, _internal))
+                        childNamespaces.extend(ns.listNamespaces(recursive, internal))
                     except: pass
                 namespaces.extend(childNamespaces)
         finally:
@@ -250,9 +251,9 @@ def listNamespaces_old():
     except:
         return []
 
-def listNamespaces(root=None, recursive=False, _internal=False):
+def listNamespaces(root=None, recursive=False, internal=False):
     """Returns a list of the namespaces in the scene"""
-    return Namespace(root or ":").listNamespaces(recursive, _internal)
+    return Namespace(root or ":").listNamespaces(recursive, internal)
 
 
 def namespaceInfo(*args, **kwargs):
@@ -882,7 +883,7 @@ class FileReference(object):
     Use listRefences command to return a list of references as instances of the FileReference class.
 
     It is important to note that instances of this class will have their copy number stripped off
-    and stored in an _internal variable upon creation.  This is to maintain compatibility with the numerous methods
+    and stored in an internal variable upon creation.  This is to maintain compatibility with the numerous methods
     inherited from the path class which requires a real file path. When calling built-in methods of FileReference,
     the path will automatically be suffixed with the copy number before being passed to maya commands, thus ensuring
     the proper results in maya as well.
@@ -895,7 +896,7 @@ class FileReference(object):
 #    paths.  namespaces are still the one weak spot, for which we must first get a path with copy number.  the use of
 #    a refNode precludes the need for the caching system, so long as a) using referenceQuery to get file paths from a refNode
 #    provides adequate performance, and b) using referenceQuery in __init__ to get a refNode from a path is os agnostic.
-#    in general, since almost all the _internal queries use the refNode,
+#    in general, since almost all the internal queries use the refNode,
 #    there should be little need for the paths, except for displaying to the user.
 
     def __init__(self, pathOrRefNode=None, namespace=None, refnode=None, unresolvedPath=None):
