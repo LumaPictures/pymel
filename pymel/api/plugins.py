36d3720736226fc1e3661030c9fde0e68e8a0765
Chad Dombrova <chadrik@gmail.com>
2010-04-13 09:13:10 -0700

    added LocatorNode mpx class.  this is just a proof of concept.  this, along with the rest of MPx classes, will eventually be accomplished through mixin classes.

--- pymel/api/plugins.py
+++ pymel/api/plugins.py
@@ -12,14 +12,14 @@ testCmd.deregister()
 
 import sys
 import inspect
-
+import maya.OpenMaya as om
 import maya.OpenMayaMPx as mpx
 import maya.cmds
 
 
 
-global registeredCommands
-registeredCommands = []
+global registered
+registered = []
 
 def _pluginModule():
     return inspect.getmodule( lambda: None )
@@ -44,52 +44,115 @@ def _unloadPlugin():
         maya.cmds.unloadPlugin( thisFile )
 
 
-def _getPlugin():
-    _loadPlugin()
-    mobject = mpx.MFnPlugin.findPlugin( _pluginName() )
-    return mpx.MFnPlugin(mobject)
-
+def _getPlugin(object=None):
+    if object is None:
+        _loadPlugin()
+        mobject = mpx.MFnPlugin.findPlugin( _pluginName() )
+        plugin = mpx.MFnPlugin(mobject)
+    elif isinstance(object, om.MObject):
+        plugin = mpx.MFnPlugin(object)
+    elif isinstance(object, mpx.MFnPlugin):
+        plugin = object
+    else:
+        raise TypeError('expected an MFnPlugin instance or an MObject that can be cast to an MFnPlugin')
+    return plugin    
+            
 class Command(mpx.MPxCommand):
-    registeredCommands = []
+    _name = None
     def __init__(self):
         mpx.MPxCommand.__init__(self)
-
+    
     @classmethod
-    def creator(cls):
+    def create(cls):
         return mpx.asMPxPtr( cls() )
 
     @classmethod
-    def register(cls, object=None):
+    def createSyntax(cls):
+        return om.MSyntax()
+    
+    @classmethod
+    def register(cls, plugin=None):
         """
-        by default the command will be registered to a dummy plugin provided by pymel.
+        By default the command will be registered to a dummy plugin provided by pymel.
 
-        If you
-        if using from within a plugin's initializePlugin or uninitializePlugin callback, pass along the
-        MObject given to these functions
+        If using from within a plugin module's ``initializePlugin`` or ``uninitializePlugin`` callback, pass along the
+        MObject given to these functions.
         """
-        if object is None:
-            plugin = _getPlugin()
-            cls.registeredCommands.append( cls.__name__ )
-        else:
-            plugin = mpx.MFnPlugin(object)
-        if hasattr(cls, 'createSyntax'):
-            plugin.registerCommand( cls.__name__, cls.creator, cls.createSyntax )
-        else:
-            plugin.registerCommand( cls.__name__, cls.creator )
+        global registered
+        name = cls._name if cls._name else cls.__name__
+        mplugin = _getPlugin(plugin)
+        mplugin.registerCommand( name, cls.create, cls.createSyntax )
+        if plugin is None:
+            registered.append( cls )
 
     @classmethod
-    def deregister(cls, object=None):
+    def deregister(cls, plugin=None):
         """
-        if using from within a plugin's initializePlugin or uninitializePlugin callback, pass along the
-        MObject given to these functions
+        If using from within a plugin module's ``initializePlugin`` or ``uninitializePlugin`` callback, pass along the
+        MObject given to these functions.
         """
-        if object is None:
-            plugin = _getPlugin()
-            cls.registeredCommands.pop(cls.__name__)
-        else:
-            plugin = mpx.MFnPlugin(object)
-        plugin.deregisterCommand( cls.__name__ )
+        global registered
+        name = cls._name if cls._name else cls.__name__
+        mplugin = _getPlugin(plugin)
+        mplugin.deregisterCommand( name )
+        if plugin is None:
+            registered.pop(cls)
 
+class LocatorNode(mpx.MPxLocatorNode):
+    _typeName = None
+    _typeId = None
+    _type = mpx.MPxNode.kLocatorNode
+    def __init__(self):
+        mpx.MPxLocatorNode.__init__(self)
+
+    @classmethod
+    def name(cls):
+        if cls._name:
+            return cls._name
+        else:
+            return cls.__name__
+    
+    @classmethod
+    def create(cls):
+        return mpx.asMPxPtr( cls() )
+    
+    @classmethod
+    def initialize(cls):
+        return
+    
+    @classmethod
+    def register(cls, plugin=None):
+        """
+        By default the command will be registered to a dummy plugin provided by pymel.
+
+        If using from within a plugin module's ``initializePlugin`` or ``uninitializePlugin`` callback, pass along the
+        MObject given to these functions.
+        """
+        global registered
+        name = cls._typeName if cls._typeName else cls.__name__
+        mplugin = _getPlugin(plugin)
+        mplugin.registerNode( name, cls._typeId, cls.create, cls.initialize, cls._type )
+        if plugin is None:
+            registered.append( cls )
+            import pymel.core.nodetypes as nodetypes
+            import pymel.internal.factories as factories
+            factories.addCustomPyNode(nodetypes, name)
+
+    @classmethod
+    def deregister(cls, plugin=None):
+        """
+        If using from within a plugin module's ``initializePlugin`` or ``uninitializePlugin`` callback, pass along the
+        MObject given to these functions.
+        """
+        global registered
+        _getPlugin(plugin).deregisterNode( cls._typeId )
+        if plugin is None:
+            registered.pop(cls)
+            name = cls._typeName if cls._typeName else cls.__name__
+            import pymel.core.nodetypes as nodetypes
+            import pymel.internal.factories as factories
+            factories.removePyNode(nodetypes, name)
+                     
 # allow this file to be loaded as its own dummy plugin
 # Initialize the script plug-in
 def initializePlugin(mobject):
@@ -106,25 +169,26 @@ def uninitializePlugin(mobject):
     #we need to get the correct module first
 
     # FIXME: determine a reliable way to get this module's name when it is being execfile'd
+    global registered
     mod = sys.modules['pymel.api.plugins']
 
     plugin = mpx.MFnPlugin(mobject)
-    for cmd in mod.Command.registeredCommands:
-        print "deregistering", cmd
-        plugin.deregisterCommand(cmd)
-    registeredCommands = []
+    for obj in mod.Command.registered:
+        print "deregistering", obj.name()
+        obj.deregisterCommand(plugin)
+    registered = []
 
 #def _repoplulate():
 #    print "repopulate"
 #    try:
-#        global registeredCommands
+#        global registered
 #        commands = maya.cmds.pluginInfo(_pluginName(), query=1, command=1)
-#        registeredCommands = registeredCommands
+#        registered = registered
 #    except:
 #        pass
 #
 #_repoplulate()
 
 
-# when we reload, should we deregister all plugins??? or maybe we can just repopulate registeredCommands
+# when we reload, should we deregister all plugins??? or maybe we can just repopulate registered
 #_unloadPlugin()
