46a97503f6f5ed30f7780fe88ba589bf53f8b469
Paul Molodowitch <elrond79@gmail.com>
2010-11-21 21:11:55 -0800

    inheritance: added handy compareDicts method for seeing differences in cascading dicts

--- maintenance/inheritance.py
+++ maintenance/inheritance.py
@@ -216,14 +216,67 @@ old_buildClass = pymel.internal.apicache.ApiCache._buildApiClassInfo
 pymel.internal.apicache.ApiCache.save = dummyFunc
 pymel.internal.apicache.ApiCache._buildApiClassInfo = dummyFunc
 
+# cache1 will hold results from loading the cache on disk
+# cache2 will hold results from rebuilding
+#    (with exception of apiClassInfo, which is copied from cache1)
+
 cache1 = pymel.internal.apicache.ApiCache()
 cache1.build()
+for mayaType, apiType in cache1.mayaTypesToApiTypes.iteritems() :
+    cache1.addMayaType( mayaType, apiType )
 
 cache2 = pymel.internal.apicache.ApiCache()
 cache2.apiClassInfo = cache1.apiClassInfo
 cache2.rebuild()
 cache2._subCaches[pymel.internal.apicache.ApiMelBridgeCache].build()
 
+def compareDicts(dict1, dict2, showDiff=True, showOnlys=False, indent=0):
+    if isinstance(dict1, (list, tuple)):
+        dict1 = dict(enumerate(diff1))
+    if isinstance(dict2, (list, tuple)):
+        dict2 = dict(enumerate(diff2))
+    v1 = set(dict1)
+    v2 = set(dict2)
+    both = v1 & v2
+    only1 = v1 - both
+    only2 = v2 - both
+    print "\t" * indent, "both:", len(both)
+    print "\t" * indent, "only1:", len(only1)
+    print "\t" * indent, "only2:", len(only2)
+    
+    differences = {}
+    for mayaType in both:
+        if dict1[mayaType] != dict2[mayaType]:
+            differences[mayaType] = (dict1[mayaType], dict2[mayaType])
+    print "\t" * indent, "differences:", len(differences)
+    
+    #print "\t" * indent, "*" * 60
+    if showDiff and differences:
+        print "\t" * indent, "different: (%d)" % len(differences)
+        for key in sorted(differences):
+            print "\t" * indent, key, ':',
+            diff1, diff2 = differences[key]
+            subDict1 = subDict2 = None
+            if type(diff1) == type(diff2) and isinstance(diff1, (dict, list, tuple)):
+                print
+                compareDicts(diff1, diff2, showDiff=showDiff, showOnlys=showOnlys, indent=indent+1)
+            else:
+                print diff1, '-', diff2
+        #print "\t" * indent, "*" * 60
+    if showOnlys:
+        if only1:
+            print "\t" * indent, "only1: (%d)" % len(only1)
+            for x in only1:
+                print "\t" * indent, x
+            #print "\t" * indent, "*" * 60
+        if only2:
+            print "\t" * indent, "only2: (%d)" % len(only2)
+            for x in only2:
+                print "\t" * indent, x
+    #print "\t" * indent, "*" * 60
+    return both, only1, only2, differences
+
+unequals = {}
 names = cache1.cacheNames() + cache1.EXTRA_GLOBAL_NAMES
 for name in names:
     val1 = getattr(cache1, name)
@@ -231,41 +284,17 @@ for name in names:
     areEqual = val1 == val2
     print "%s equal? %s" % (name,  areEqual ),
     if not areEqual:
+        unequals[name] = (val1, val2)
         print "(%d, %d)" % (len(val1), len(val2)),
     print
 
-dict1 = cache1.mayaTypesToApiTypes
-dict2 = cache2.mayaTypesToApiTypes
-v1 = set(dict1)
-v2 = set(dict2)
-both = v1 & v2
-only1 = v1 - both
-only2 = v2 - both
-print "both:", len(both)
-print "only1:", len(only1)
-print "only2:", len(only2)
+for key, (val1, val2) in unequals.iteritems():
+    print '*' * 80
+    print key
+    compareDicts(val1, val2, showDiff=True)
+    print key
+    print '*' * 80
 
-differences = {}
-for mayaType in both:
-    if dict1[mayaType] != dict2[mayaType]:
-        differences[mayaType] = (dict1[mayaType], dict2[mayaType])
-
-print "*" * 60
-print "different: (%d)" % len(differences)
-for key, val in differences:
-    print
-print "*" * 60
-print "only1: (%d)" % len(only1)
-for x in only1:
-    print x
-print "*" * 60
-print "only2: (%d)" % len(only2)
-for x in only2:
-    print x
-print "*" * 60
-
-import pymel.internal.factories
-len(pymel.internal.factories._apiCacheInst.apiTypesToMayaTypes)
 
 pymel.internal.apicache.ApiCache.save = old_save
 pymel.internal.apicache.ApiCache._buildApiClassInfo = old_buildClass
