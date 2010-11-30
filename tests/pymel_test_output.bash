e149dd4355c1c6f998e3a1ac0ed836fb79d8680c
Paul Molodowitch <elrond79@gmail.com>
2010-11-29 19:44:49 -0800

    added pymel_test_output for easier command-line running of nosetests

---
+++ tests/pymel_test_output.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mv ~/maya ~/maya.bak
+
+this_script=$(python -c "import os; print os.path.normcase(os.path.normpath(os.path.realpath(os.path.abspath('''$0'''))))")
+this_dir=$(dirname "$this_script")
+
+nose_dir=$(dirname $(dirname $(python -c "import nose; print nose.__file__")))
+mayapy_dir=$(dirname $(which mayapy))
+
+
+THE_CMD="export DISPLAY=:0.0;export HOME=$HOME;export TERM=$TERM;export SHELL=$SHELL;export USER=$USER;export PATH="'$PATH'":$mayapy_dir;export PYTHONPATH="'$PYTHONPATH'":$nose_dir;${this_dir}/pymel_test.py"' $@ 2>&1 | tee pymelTestOut.txt'
+
+echo $THE_CMD
+env -i bash -c "$THE_CMD"
+
+rm -rf ~/maya
+mv ~/maya.bak ~/maya
