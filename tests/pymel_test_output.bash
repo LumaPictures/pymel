b525f3669690b1ffb021b69f6899ec521988f51a
Paul Molodowitch <elrond79@gmail.com>
2010-11-29 21:39:14 -0800

    pymel_test_output: added pymel dir to pythonpath

--- tests/pymel_test_output.bash
+++ tests/pymel_test_output.bash
@@ -5,11 +5,12 @@ mv ~/maya ~/maya.bak
 this_script=$(python -c "import os; print os.path.normcase(os.path.normpath(os.path.realpath(os.path.abspath('''$0'''))))")
 this_dir=$(dirname "$this_script")
 
+pymel_dir=$(dirname $this_dir)
 nose_dir=$(dirname $(dirname $(python -c "import nose; print nose.__file__")))
 mayapy_dir=$(dirname $(which mayapy))
 
 
-THE_CMD="export DISPLAY=:0.0;export HOME=$HOME;export TERM=$TERM;export SHELL=$SHELL;export USER=$USER;export PATH="'$PATH'":$mayapy_dir;export PYTHONPATH="'$PYTHONPATH'":$nose_dir;${this_dir}/pymel_test.py"' $@ 2>&1 | tee pymelTestOut.txt'
+THE_CMD="export DISPLAY=:0.0;export HOME=$HOME;export TERM=$TERM;export SHELL=$SHELL;export USER=$USER;export PATH="'$PATH'":$mayapy_dir;export PYTHONPATH=$pymel_dir:"'$PYTHONPATH'":$nose_dir;${this_dir}/pymel_test.py"' $@ 2>&1 | tee pymelTestOut.txt'
 
 echo $THE_CMD
 env -i bash -c "$THE_CMD"
