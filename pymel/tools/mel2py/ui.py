f6d70893eebf7eabfebede792fd2389ea773095d
koreno <koreno@gmail.com>
2010-12-03 18:31:20 +0200

    cleanup

--- pymel/tools/mel2py/ui.py
+++ pymel/tools/mel2py/ui.py
@@ -1,4 +1,6 @@
-from pymel.core.windows import *
+from pymel.core.windows import window, cmdScrollFieldExecuter, informBox, horizontalLayout, button
+from pymel.core.uitypes import Window
+from pymel.tools.mel2py import mel2pyStr
 
 class MelToPythonWindow(Window):
 
@@ -6,30 +8,28 @@ class MelToPythonWindow(Window):
         self = window(title=name or "Mel To Python")
         return Window.__new__(cls, self)
 
-    def convert(w):
-        from .. import mel2pyStr
-        if cmds.cmdScrollFieldExecuter(w.mel,q=1,hasSelection=1):
-            cmds.cmdScrollFieldExecuter(w.mel,e=1,copySelection=1)
-            cmds.cmdScrollFieldExecuter(w.python,e=1,clear=1)
-            cmds.cmdScrollFieldExecuter(w.python,e=1,pasteSelection=1)
-            mel = cmds.cmdScrollFieldExecuter(w.python,q=1,text=1)
+    def convert(self, *x):
+        
+        if self.mel.getHasSelection():
+            self.mel.copySelection()
+            self.python.clear()
+            self.python.pasteSelection()
+            mel = self.python.getText()
         else:
-            mel = cmds.cmdScrollFieldExecuter(w.mel,q=1,text=1)
+            mel = self.mel.getText()
         try:
             py = mel2pyStr(mel)
         except Exception, e:
-            confirmDialog(t="Mel To Python",m="Conversion Error:\n%s" % e,b=["Ok"], db="Ok")
+            informBox("Mel To Python","Conversion Error:\n%s" % e)
         else:
-            cmds.cmdScrollFieldExecuter(w.python,e=1,text=py)
+            self.python.setText(py)
 
 
     def __init__(self):
-        formLayout(slc=True, ratios=[1,.1,1], orientation=FormLayout.HORIZONTAL, childCreators = [
-              cmdScrollFieldExecuter("mel", slc=True),
-              button("button", slc=True, l="->", c=lambda *x: self.convert(), bgc=[.5,.7,1]),
-              cmdScrollFieldExecuter("python", slc=True, st="python")
-              ]).create(self.__dict__,parent=self,debug=1)
-
+        with horizontalLayout(ratios=[1,.1,1]):
+            self.mel = cmdScrollFieldExecuter()
+            self.button = button(l="->", c=self.convert, bgc=[.5,.7,1])
+            self.python = cmdScrollFieldExecuter(st="python")
         self.setWidthHeight([600,800])
         self.show()
 
