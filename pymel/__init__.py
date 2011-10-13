29e7c2d4f568850f43c5d7aabc5025d07f92c2ff
Paul Molodowitch <elrond79@gmail.com>
2011-10-12 19:18:06 -0700

    changed version to 1.0.4b2

--- pymel/__init__.py
+++ pymel/__init__.py
@@ -23,7 +23,7 @@ Attitude Studio, and ImageMovers Digital.
 """
 
 
-__version__ = '1.0.3'
+__version__ = '1.0.4b2'
 __authors__ = ['Chad Dombrova', 'Olivier Renouard', 'Ofer Koren', 'Paul Molodowitch']
 
 import sys
