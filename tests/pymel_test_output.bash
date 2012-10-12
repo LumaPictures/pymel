8fd3072f9ceb58583a90ad2a78f493369c2fdd72
Paul Molodowitch <elrond79@gmail.com>
2012-10-11 20:28:45 -0700

    pymel_test_output.bash: set MAYA_APP_DIR instead of moving around the maya settings dir

--- tests/pymel_test_output.bash
+++ tests/pymel_test_output.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-mv ~/maya ~/maya.bak
+settings_dir=~/maya_pymel_test
+
+#if [[ -d "$settings_dir" ]]; then
+#    rm -rf "$settings_dir"
+#fi
+mkdir -p $settings_dir
 
 this_script=$(python -c "import os; print os.path.normcase(os.path.normpath(os.path.realpath(os.path.abspath('''$0'''))))")
 this_dir=$(dirname "$this_script")
@@ -10,10 +15,7 @@ nose_dir=$(dirname $(dirname $(python -c "import nose; print nose.__file__")))
 mayapy_dir=$(dirname $(which mayapy))
 
 
-THE_CMD="export DISPLAY=:0.0;export HOME=$HOME;export TERM=$TERM;export SHELL=$SHELL;export USER=$USER;export PATH="'$PATH'":$mayapy_dir;export PYTHONPATH=$pymel_dir:"'$PYTHONPATH'":$nose_dir;${this_dir}/pymel_test.py $@ 2>&1 | tee pymelTestOut.txt"
+THE_CMD="export DISPLAY=:0.0;export HOME=$HOME;export TERM=$TERM;export SHELL=$SHELL;export USER=$USER;export MAYA_APP_DIR="'"'"$settings_dir"'"'";export PATH="'$PATH'":$mayapy_dir;export PYTHONPATH=$pymel_dir:"'$PYTHONPATH'":$nose_dir;${this_dir}/pymel_test.py $@ 2>&1 | tee pymelTestOut.txt"
 
 echo $THE_CMD
 env -i bash -c "$THE_CMD"
-
-rm -rf ~/maya
-mv ~/maya.bak ~/maya
