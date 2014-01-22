99d073d70fd56d6b6362988db524637573bcec79
Paul Molodowitch <elrond79@gmail.com>
2014-01-22 15:31:19 -0800

    test_mayaBugs: change failure conditions, so it alerts us if failure/success status has CHANGED from what's expected (so we know when bugs have been fixed)

--- tests/test_mayaBugs.py
+++ tests/test_mayaBugs.py
@@ -1,10 +1,12 @@
 import sys
+import os
 import unittest
 import maya.cmds as cmds
 import maya.OpenMaya as om
 import maya.OpenMayaAnim as oma
 import maya.OpenMayaFX as omfx
 
+import pymel.versions
 from pymel.util.testing import TestCaseExtended
 
 if not hasattr(cmds, 'about'):
@@ -14,6 +16,10 @@ if not hasattr(cmds, 'about'):
 #===============================================================================
 # Current Bugs
 #===============================================================================
+
+# For CURRENT bugs, we PASS is the bug is still present, and FAIL if it goes
+# away... this may be counter-intuitive, but it acts as an alert if a bug is
+# fixed (so we can possibly get rid of yucky work-around code...)
 
 # Bug report 378211
 class TestConstraintAngleOffsetQuery(TestCaseExtended):
@@ -31,7 +37,16 @@ class TestConstraintAngleOffsetQuery(TestCaseExtended):
             setVals = (12, 8, 7)
             cmd(constraint, e=1, offset=setVals)
             getVals = tuple(cmd(constraint, q=1, offset=1))
-            self.assertVectorsEqual(setVals, getVals)
+
+            # self.assertVectorsEqual(setVals, getVals)
+
+            # check that things are BAD!
+            try:
+                self.assertVectorsEqual(setVals, getVals)
+            except AssertionError:
+                pass
+            else:
+                self.fail("TestConstraintAngleOffsetQuery was fixed! Huzzah!")
 
 # Bug report 378192
 class TestEmptyMFnNurbsCurve(unittest.TestCase):
@@ -47,10 +62,20 @@ class TestEmptyMFnNurbsCurve(unittest.TestCase):
 
         mnc = om.MFnNurbsCurve()
         self.assertTrue(mnc.hasObj(node))
+#         try:
+#             mnc.setObject(node)
+#         except Exception:
+#             self.fail("MFnNurbs curve doesn't work with empty curve object")
+
+        # check that things are BAD!
         try:
             mnc.setObject(node)
         except Exception:
-            self.fail("MFnNurbs curve doesn't work with empty curve object")
+            pass
+        else:
+            self.fail("MFnNurbs curve now works with empty curve objects! Yay!")
+
+
 
 # Bug report 344037
 class TestSurfaceRangeDomain(unittest.TestCase):
@@ -58,64 +83,70 @@ class TestSurfaceRangeDomain(unittest.TestCase):
         cmds.file(new=1, f=1)
 
     def runTest(self):
-        # create a nurbs sphere
-        mySphere = cmds.sphere()[0]
-        # a default sphere should have u/v
-        # parameter ranges of 0:4/0:8
+        try:
+            # create a nurbs sphere
+            mySphere = cmds.sphere()[0]
+            # a default sphere should have u/v
+            # parameter ranges of 0:4/0:8
 
-        # The following selections should
-        # result in one of these:
-        desiredResults = ('nurbsSphere1.u[2:3][0:8]',
-                          'nurbsSphere1.u[2:3][*]',
-                          'nurbsSphere1.u[2:3]',
-                          'nurbsSphere1.uv[2:3][0:8]',
-                          'nurbsSphere1.uv[2:3][*]',
-                          'nurbsSphere1.uv[2:3]',
-                          'nurbsSphere1.v[0:8][2:3]',
-                          'nurbsSphere1.v[*][2:3]')
+            # The following selections should
+            # result in one of these:
+            desiredResults = ('nurbsSphere1.u[2:3][0:8]',
+                              'nurbsSphere1.u[2:3][*]',
+                              'nurbsSphere1.u[2:3]',
+                              'nurbsSphere1.uv[2:3][0:8]',
+                              'nurbsSphere1.uv[2:3][*]',
+                              'nurbsSphere1.uv[2:3]',
+                              'nurbsSphere1.v[0:8][2:3]',
+                              'nurbsSphere1.v[*][2:3]')
 
 
-        # Passes
-        cmds.select('nurbsSphere1.u[2:3][*]')
-        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+            # Passes
+            cmds.select('nurbsSphere1.u[2:3][*]')
+            self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
 
-        # Passes
-        cmds.select('nurbsSphere1.v[*][2:3]')
-        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+            # Passes
+            cmds.select('nurbsSphere1.v[*][2:3]')
+            self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
 
-        # Fails! - returns 'nurbsSphere1.u[2:3][0:1]'
-        cmds.select('nurbsSphere1.u[2:3]')
-        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+            # Fails! - returns 'nurbsSphere1.u[2:3][0:1]'
+            cmds.select('nurbsSphere1.u[2:3]')
+            self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
 
-        # Fails! - returns 'nurbsSphere1.u[2:3][0:1]'
-        cmds.select('nurbsSphere1.uv[2:3][*]')
-        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+            # Fails! - returns 'nurbsSphere1.u[2:3][0:1]'
+            cmds.select('nurbsSphere1.uv[2:3][*]')
+            self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
 
-        # The following selections should
-        # result in one of these:
-        desiredResults = ('nurbsSphere1.u[0:4][2:3]',
-                          'nurbsSphere1.u[*][2:3]',
-                          'nurbsSphere1.uv[0:4][2:3]',
-                          'nurbsSphere1.uv[*][2:3]',
-                          'nurbsSphere1.v[2:3][0:4]',
-                          'nurbsSphere1.v[2:3][*]',
-                          'nurbsSphere1.v[2:3]')
+            # The following selections should
+            # result in one of these:
+            desiredResults = ('nurbsSphere1.u[0:4][2:3]',
+                              'nurbsSphere1.u[*][2:3]',
+                              'nurbsSphere1.uv[0:4][2:3]',
+                              'nurbsSphere1.uv[*][2:3]',
+                              'nurbsSphere1.v[2:3][0:4]',
+                              'nurbsSphere1.v[2:3][*]',
+                              'nurbsSphere1.v[2:3]')
 
-        # Passes
-        cmds.select('nurbsSphere1.u[*][2:3]')
-        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+            # Passes
+            cmds.select('nurbsSphere1.u[*][2:3]')
+            self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
 
-        # Passes
-        cmds.select('nurbsSphere1.v[2:3][*]')
-        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+            # Passes
+            cmds.select('nurbsSphere1.v[2:3][*]')
+            self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
 
-        # Fails! - returns 'nurbsSphereShape1.u[0:1][2:3]'
-        cmds.select('nurbsSphere1.v[2:3]')
-        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+            # Fails! - returns 'nurbsSphereShape1.u[0:1][2:3]'
+            cmds.select('nurbsSphere1.v[2:3]')
+            self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
 
-        # Fails! - returns 'nurbsSphereShape1.u[0:4][0:1]'
-        cmds.select('nurbsSphere1.uv[*][2:3]')
-        self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+            # Fails! - returns 'nurbsSphereShape1.u[0:4][0:1]'
+            cmds.select('nurbsSphere1.uv[*][2:3]')
+            self.assertTrue(cmds.ls(sl=1)[0] in desiredResults)
+        except AssertionError:
+            pass
+        else:
+            # check that things are BAD!
+            self.fail("Nurbs surface range domain bug fixed!")
 
 
 # Bug report 345384
@@ -137,47 +168,57 @@ class TestMMatrixSetAttr(unittest.TestCase):
         cmds.file(new=1, f=1)
 
     def runTest(self):
-        class MyClass1(object):
-            def __init__(self):
-                self._bar = 'not set'
+        # We expect it to fail on windows, and pass on other operating systems...
+        shouldPass = os.name != 'nt'
+        try:
+            class MyClass1(object):
+                def __init__(self):
+                    self._bar = 'not set'
 
-            def _setBar(self, val):
-                print "setting bar to:", val
-                self._bar = val
-            def _getBar(self):
-                print "getting bar..."
-                return self._bar
-            bar = property(_getBar, _setBar)
+                def _setBar(self, val):
+                    print "setting bar to:", val
+                    self._bar = val
+                def _getBar(self):
+                    print "getting bar..."
+                    return self._bar
+                bar = property(_getBar, _setBar)
 
-            # These two are just so we can trace what's going on...
-            def __getattribute__(self, name):
-                # don't just use 'normal' repr, as that will
-                # call __getattribute__!
-                print "__getattribute__(%s, %r)" % (object.__repr__(self), name)
-                return super(MyClass1, self).__getattribute__(name)
-            def __setattr__(self, name, val):
-                print "__setattr__(%r, %r, %r)" % (self, name, val)
-                return super(MyClass1, self).__setattr__(name, val)
+                # These two are just so we can trace what's going on...
+                def __getattribute__(self, name):
+                    # don't just use 'normal' repr, as that will
+                    # call __getattribute__!
+                    print "__getattribute__(%s, %r)" % (object.__repr__(self), name)
+                    return super(MyClass1, self).__getattribute__(name)
+                def __setattr__(self, name, val):
+                    print "__setattr__(%r, %r, %r)" % (self, name, val)
+                    return super(MyClass1, self).__setattr__(name, val)
 
-        foo1 = MyClass1()
-        # works like we expect...
-        foo1.bar = 7
-        print "foo1.bar:", foo1.bar
-        self.assertTrue(foo1.bar == 7)
+            foo1 = MyClass1()
+            # works like we expect...
+            foo1.bar = 7
+            print "foo1.bar:", foo1.bar
+            self.assertTrue(foo1.bar == 7)
 
 
-        class MyClass2(MyClass1, om.MMatrix): pass
+            class MyClass2(MyClass1, om.MMatrix): pass
 
-        foo2 = MyClass2()
-        foo2.bar = 7
-        # Here, on windows, MMatrix's __setattr__ takes over, and
-        # (after presumabably determining it didn't need to do
-        # whatever special case thing it was designed to do)
-        # instead of calling the super's __setattr__, which would
-        # use the property, inserts it into the object's __dict__
-        # manually
-        print "foo2.bar:", foo2.bar
-        self.assertTrue(foo2.bar == 7)
+            foo2 = MyClass2()
+            foo2.bar = 7
+            # Here, on windows, MMatrix's __setattr__ takes over, and
+            # (after presumabably determining it didn't need to do
+            # whatever special case thing it was designed to do)
+            # instead of calling the super's __setattr__, which would
+            # use the property, inserts it into the object's __dict__
+            # manually
+            print "foo2.bar:", foo2.bar
+            self.assertTrue(foo2.bar == 7)
+        except Exception:
+            if shouldPass:
+                raise
+        else:
+            if not shouldPass:
+                self.fail("MMatrix setattr bug seems to have been fixed!")
+
 
     def tearDown(self):
         # Restore the 'fixed' __setattr__'s
@@ -185,37 +226,42 @@ class TestMMatrixSetAttr(unittest.TestCase):
             om.MMatrix.__setattr__ = self.fixedSetAttr
 
 # Introduced in maya 2014
-class TestShapeParentInstance(unittest.TestCase):
-    def setUp(self):
-        cmds.file(new=1, f=1)
+if pymel.versions.current() >= pymel.versions.v2014:
+    class TestShapeParentInstance(unittest.TestCase):
+        def setUp(self):
+            cmds.file(new=1, f=1)
 
-    def runTest(self):
-        import maya.cmds as cmds
+        def runTest(self):
+            try:
+                import maya.cmds as cmds
 
-        def getShape(trans):
-            return cmds.listRelatives(trans, children=True, shapes=True)[0]
+                def getShape(trans):
+                    return cmds.listRelatives(trans, children=True, shapes=True)[0]
 
-        cmds.file(new=1, f=1)
-        shapeTransform = cmds.polyCube(name='singleShapePoly')[0]
-        origShape = getShape(shapeTransform)
-        dupeTransform1 = cmds.duplicate(origShape, parentOnly=1)[0]
-        cmds.parent(origShape, dupeTransform1, shape=True, addObject=True, relative=True)
-        dupeTransform2 = cmds.duplicate(dupeTransform1)[0]
-        cmds.delete(dupeTransform1)
-        dupeShape = getShape(dupeTransform2)
+                cmds.file(new=1, f=1)
+                shapeTransform = cmds.polyCube(name='singleShapePoly')[0]
+                origShape = getShape(shapeTransform)
+                dupeTransform1 = cmds.duplicate(origShape, parentOnly=1)[0]
+                cmds.parent(origShape, dupeTransform1, shape=True, addObject=True, relative=True)
+                dupeTransform2 = cmds.duplicate(dupeTransform1)[0]
+                cmds.delete(dupeTransform1)
+                dupeShape = getShape(dupeTransform2)
 
-        # In maya 2014, this raises:
-        # Error: Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Source is not connected.
-        # Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Destination attribute must be writable.
-        # Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Destination attribute must be writable.
-        # Traceback (most recent call last):
-        # File "<maya console>", line 13, in <module>
-        # RuntimeError: Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Source is not connected.
-        # Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Destination attribute must be writable.
-        # Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Destination attribute must be writable. #
+                # In maya 2014, this raises:
+                # Error: Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Source is not connected.
+                # Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Destination attribute must be writable.
+                # Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Destination attribute must be writable.
+                # Traceback (most recent call last):
+                # File "<maya console>", line 13, in <module>
+                # RuntimeError: Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Source is not connected.
+                # Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Destination attribute must be writable.
+                # Connection not made: 'singleShapePolyShape2.instObjGroups[1]' -> 'initialShadingGroup.dagSetMembers[2]'. Destination attribute must be writable. #
 
-        cmds.parent(dupeShape, shapeTransform, shape=True, addObject=True, relative=True)
-
+                cmds.parent(dupeShape, shapeTransform, shape=True, addObject=True, relative=True)
+            except Exception:
+                pass
+            else:
+                self.fail("ShapeParentInstance bug fixed!")
 
 
 #===============================================================================
