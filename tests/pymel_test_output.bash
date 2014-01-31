abb8ed3e7582939e0092c134036806ed0ae31029
Paul Molodowitch <pm@stanfordalumni.org>
2014-01-30 16:11:24 -0800

    pymel_test_output.bash: tweaks to get it (closer to) working on windows

--- tests/pymel_test_output.bash
+++ tests/pymel_test_output.bash
@@ -7,23 +7,30 @@ settings_dir=~/maya_pymel_test
 #fi
 mkdir -p $settings_dir
 
-this_script=$(python -c "import os; print os.path.normcase(os.path.normpath(os.path.realpath(os.path.abspath('''$0'''))))")
-this_dir=$(dirname "$this_script")
+this_script=$(python -c "import os; import posixpath; print os.path.normcase(os.path.normpath(os.path.realpath(os.path.abspath('''$0''')))).replace(os.sep, posixpath.sep)")
+echo "$this_script"
 
-pymel_dir=$(dirname $this_dir)
-nose_dir=$(dirname $(dirname $(python -c "import nose; print nose.__file__")))
-unittest2_dir=$(dirname $(dirname $(python -c '''
+this_dir=$(dirname "$this_script")
+echo "$this_dir"
+pymel_dir=$(dirname "$this_dir")
+nose_dir=$(dirname "$(dirname "$(python -c 'import nose; print nose.__file__')")")
+unittest2_dir=$(dirname "$(dirname "$(python -c '''
 try:
     import unittest2
 except ImportError:
     pass
 else:
-    print unittest2.__file__''')))
+    print unittest2.__file__''')")")
     
-mayapy_dir=$(dirname $(which mayapy))
+mayapy_dir=$(dirname "$(which mayapy)")
 
 
 THE_CMD="export DISPLAY=:0.0;export HOME=$HOME;export TERM=$TERM;export SHELL=$SHELL;export USER=$USER;export PATH="'$PATH'":$mayapy_dir;export PYTHONPATH='$pymel_dir:$nose_dir:$unittest2_dir';${this_dir}/pymel_test.py --app-dir='$settings_dir' $@ 2>&1 | tee pymelTestOut.txt"
 
 echo $THE_CMD
-env -i bash -c "$THE_CMD"
+
+# for some reason, just doing:
+#    env -i "$bash_cmd" -c "$THE_CMD"
+# ...doesn't work in cygwin
+bash_cmd=$(which bash)
+env -i "$bash_cmd" -c "$THE_CMD"
