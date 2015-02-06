f601afda783fef02602bded14b01fb6c5c448c37
Chad Dombrova <chadrik@gmail.com>
2015-02-06 11:24:21 -0800

    docs: make prettier

--- pymel/tools/__init__.py
+++ pymel/tools/__init__.py
@@ -4,9 +4,9 @@ Extensions, integrations, and pymel-dependent tools.
 Tool Modules
 ------------
 
-  - `pymel.tools.envparse`
   - `pymel.tools.loggingControl`
   - `pymel.tools.mel2py`
   - `pymel.tools.py2mel`
+  - `pymel.tools.envparse`
 
 """
