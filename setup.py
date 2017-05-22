e3118820edc48e06e518cf64dc755eb47547758d
Paul Molodowitch <pm@stanfordalumni.org>
2017-05-22 15:41:29 -0700

    remove ply as requirement, as we have pymel.util.external.ply
    Also, v3.3 is no longer available on pypy, as noted by this issue:
    https://github.com/LumaPictures/pymel/issues/382

--- setup.py
+++ setup.py
@@ -176,7 +176,7 @@ def main():
     orig_script_args = setuptools.command.easy_install.get_script_args
     orig_nt_quote_arg = setuptools.command.easy_install.nt_quote_arg
 
-    requirements = ['ply==3.3', 'ipython']
+    requirements = ['ipython']
     if isdev():
         requirements.append('BeautifulSoup >3.0')
 
