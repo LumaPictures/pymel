fa3c4b516f4c162cd8249b8bc47cb4c71eeb784a
Paul Molodowitch <pm@stanfordalumni.org>
2018-11-19 12:49:27 -0800

    add reapplyApiMelBridge.py utility

---
+++ maintenance/reapplyApiMelBridge.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env mayapy
+import inspect
+import os
+import sys
+
+THIS_FILE = os.path.normpath(os.path.abspath(inspect.getsourcefile(lambda: None)))
+THIS_DIR = os.path.dirname(THIS_FILE)
+PYMEL_ROOT = os.path.dirname(THIS_DIR)
+
+sys.path.insert(0, PYMEL_ROOT)
+
+import pymel.internal.apicache as apicache
+import pymel.internal.startup
+
+import pymel.util as _util
+
+logger = pymel.internal.startup._logger
+
+bridgeCache = apicache.ApiMelBridgeCache()
+bridgeCache.load()
+print "read: {}".format(bridgeCache.path())
+
+for version in apicache.ApiCache.allVersions():
+    cacheInst = apicache.ApiCache()
+    cacheInst.version = version
+    cacheInst.load()
+
+    print "updating: {}".format(cacheInst.path())
+    _util.mergeCascadingDicts(bridgeCache.apiClassOverrides, cacheInst.apiClassInfo,
+                              allowDictToListMerging=True)
+    cacheInst.save()
+
+
