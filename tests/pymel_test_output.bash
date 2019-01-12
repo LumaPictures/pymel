8cb89726cb136c19c1921f3eb58381def33469f2
Paul Molodowitch <paulm@lumapictures.com>
2019-01-11 16:25:25 -0800

    pymel_test_output: disable CIP / CLIC_IPM for tests
    avoids a crash we got on our gitlab test runner

--- tests/pymel_test_output.bash
+++ tests/pymel_test_output.bash
@@ -38,7 +38,9 @@ unittest2_dir=$(print_python_module_dir pytest)
 
 mayapy_dir=$(dirname "$(which mayapy)")
 
-THE_CMD="export DISPLAY=:0.0;export HOME=$HOME;export TERM=$TERM;export SHELL=$SHELL;export USER=$USER;export PATH="'$PATH'":$mayapy_dir;export PYTHONPATH='$pymel_dir:$pytest_dir:$pkg_resources_dir:$nose_dir:$unittest2_dir';${this_dir}/pymel_test.py --gui-stdout --app-dir='$settings_dir' $@"
+# without setting MAYA_DISABLE_CIP and MAYA_DISABLE_CLIC_IPM, got segfaults
+# on our gitlab test runner...
+THE_CMD="export DISPLAY=:0.0;export HOME=$HOME;export TERM=$TERM;export SHELL=$SHELL;export MAYA_DISABLE_CIP=1;export MAYA_DISABLE_CLIC_IPM=1;export USER=$USER;export PATH="'$PATH'":$mayapy_dir;export PYTHONPATH='$pymel_dir:$pytest_dir:$pkg_resources_dir:$nose_dir:$unittest2_dir';${this_dir}/pymel_test.py --gui-stdout --app-dir='$settings_dir' $@"
 
 echo $THE_CMD
 
