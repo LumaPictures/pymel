788553ace9d81ccfcf0bf15f300bbad71a5442f9
Chad Dombrova <chadrik@gmail.com>
2020-01-31 15:49:12 -0800

    Update PLY to 3.11

--- pymel/util/external/ply/__init__.py
+++ pymel/util/external/ply/__init__.py
@@ -1,9 +1,5 @@
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import unicode_literals
 # PLY package
 # Author: David Beazley (dave@dabeaz.com)
 
-from builtins import *
+__version__ = '3.11'
 __all__ = ['lex','yacc']
