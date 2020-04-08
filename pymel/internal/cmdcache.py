LumaPictures/pymel

commit 9f2721b0c499e68a416b186f8086b8c2dffac640

cmdcache: don't error if expected type is str, but get unicode
pre 2020, the parsed modes for 'camera' didn't include 'edit', which caused
cmdcache:922 to not trip

pymel/internal/cmdcache.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 # Built-in imports
+from future.utils import PY2
 from builtins import range
 from past.builtins import basestring
 import os
@@ -765,6 +766,8 @@ def nodeCreationCmd(func, nodeType):
         createArgs = [argMaker() for argMaker in createArgs]
 
     # run the function
+    _logger.debug('{}(*{!r})'.format(func.__name__, createArgs))
+
     return func(*createArgs)
 
 
@@ -908,6 +911,12 @@ def testNodeCmd(funcName, cmdInfo, nodeCmd=False, verbose=False):
                     #_logger.debug('val: %r' % (val,))
                     resultType = _objectToType(val)
 
+                    # deal with unicode vs str
+                    if PY2:
+                        if set([argtype, resultType]) == set([str, unicode]):
+                            # just ignore this, set resultType to match
+                            resultType = argtype
+
                     # ensure symmetry between edit and query commands:
                     # if this flag is queryable and editable, then its queried value should be symmetric to its edit arguments
                     if 'edit' in modes and argtype != resultType:
@@ -938,7 +947,7 @@ def testNodeCmd(funcName, cmdInfo, nodeCmd=False, verbose=False):
                             flagInfo['resultNeedsCasting'] = True
                             val = argtype(val)
                         else:
-                            # no valid corrctions found
+                            # no valid corrections found
                             _logger.info(cmd)
                             _logger.info("\treturn mismatch")
                             _logger.info('\tresult: %s', val.__repr__())

commit ea0f7e2bd696d7212db29e748f41212cec703c60

cmdcache: fix resultNeedsUnpacking
this fixes bug introduced in 9f2721b0c499e68a416b186f8086b8c2dffac640
specifically, resultType may be a list - ie, [int] - in which case it's
unhashable, and can't be put in a set

pymel/internal/cmdcache.py
@@ -924,10 +924,10 @@ def testNodeCmd(funcName, cmdInfo, nodeCmd=False, verbose=False):
 
                     # deal with unicode vs str
                     if PY2:
-                        if set([argtype, resultType]) == set([str, unicode]):
-                            # just ignore this, set resultType to match
+                        if argtype in (str, unicode) \
+                                and resultType in (str, unicode):
+                            # just ignore str/unicode diff, set resultType to match
                             resultType = argtype
-
                     # ensure symmetry between edit and query commands:
                     # if this flag is queryable and editable, then its queried value should be symmetric to its edit arguments
                     if 'edit' in modes and argtype != resultType:
@@ -982,8 +982,10 @@ def testNodeCmd(funcName, cmdInfo, nodeCmd=False, verbose=False):
                         flagInfo.pop(shortname, None)
                         modes = []  # stop edit from running
                     else:
-                        _logger.info(cmd)
+                        _logger.info("Error running: {}".format(cmd))
                         _logger.info("\t" + str(msg).rstrip('\n'))
+                        import traceback
+                        _logger.debug(traceback.format_exc())
                     val = None
 
                 except RuntimeError as msg: