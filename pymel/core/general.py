LumaPictures/pymel

commit b26c41a6a29e54ce785d2f9881f66694afdcd53c

general: fix for addAttr(type=str)

pymel/core/general.py
@@ -684,15 +684,21 @@ def addAttr(*args, **kwargs):
             >>> [x.attrName() for x in PyNode('persp').listAttr() if 'autoLong2' in x.name()]
             ['autoLong2', 'autoLong2_first', 'autoLong2_second']
     """
+    # temp hack, because str is currently builtins.str...
+    if PY2:
+        from __builtin__ import str
+
     attributeTypes = ['bool', 'long', 'short', 'byte', 'char', 'enum',
                       'float', 'double', 'doubleAngle', 'doubleLinear',
                       'compound', 'message', 'time', 'fltMatrix', 'reflectance',
                       'spectrum', 'float2', 'float3', 'double2', 'double3', 'long2',
-                      'long3', 'short2', 'short3', datatypes.Vector]
+                      'long3', 'short2', 'short3', float, int, bool,
+                      datatypes.Vector]
 
     dataTypes = ['string', 'stringArray', 'matrix', 'reflectanceRGB',
                  'spectrumRGB', 'doubleArray', 'Int32Array', 'vectorArray',
-                 'nurbsCurve', 'nurbsSurface', 'mesh', 'lattice', 'pointArray']
+                 'nurbsCurve', 'nurbsSurface', 'mesh', 'lattice', 'pointArray',
+                 str]
 
     type = kwargs.pop('type', kwargs.pop('typ', None))
     childSuffixes = kwargs.pop('childSuffixes', None)
@@ -713,12 +719,15 @@ def addAttr(*args, **kwargs):
                 int: 'long',
                 bool: 'bool',
                 datatypes.Vector: 'double3',
-                str: 'string',
-                str: 'string'
             }[at]
         except KeyError:
             pass
         kwargs['at'] = at
+    dt = kwargs.pop('dataType', kwargs.pop('dt', None))
+    if dt is not None:
+        if dt is str:
+            dt = 'string'
+        kwargs['dt'] = dt
 
     if kwargs.get('e', kwargs.get('edit', False)):
         for editArg, value in kwargs.items():

tests/test_general.py
@@ -1820,6 +1820,29 @@ class test_addAttr(unittest.TestCase):
                          [u'autoLong2', u'autoLong2_first',
                           u'autoLong2_second'])
 
+    def test_pythonTypes(self):
+        for kwargType, typesAndNames in [
+            ('attributeType',
+             [
+                 (float, 'double'),
+                 (int, 'long'),
+                 (bool, 'bool'),
+                 # skip testing double3, because it's a compound attr,
+                 # and we'd have to add children...
+                 #(datatypes.Vector, 'double3'),
+             ]),
+            ('dataType',
+             [
+                 (str, 'string'),
+             ]),
+        ]:
+            for pythonType, expectedTypeName in typesAndNames:
+                for kwargName in ('type', kwargType):
+                    attrName = '{}_{}'.format(kwargName, pythonType.__name__)
+                    self.loc.addAttr(attrName, **{kwargName: pythonType})
+                    self.assertEqual(self.loc.attr(attrName).type(),
+                                     expectedTypeName)
+
 
 class test_deleteAttr(unittest.TestCase):
     def setUp(self):

commit cce908daf331f05234b1800feb4108bcd17919d9

general: fix PY3 regression introduced in b26c41a6a29e54ce785d2f9881f66694afdcd53c

pymel/core/general.py
@@ -687,6 +687,10 @@ def addAttr(*args, **kwargs):
     # temp hack, because str is currently builtins.str...
     if PY2:
         from __builtin__ import str
+    else:
+        # the same as what's at global scope, but the above import makes str
+        # a local variable - and unbound in PY3 without this!
+        from builtins import str
 
     attributeTypes = ['bool', 'long', 'short', 'byte', 'char', 'enum',
                       'float', 'double', 'doubleAngle', 'doubleLinear',