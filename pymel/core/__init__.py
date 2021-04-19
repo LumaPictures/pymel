164016f035aa6b24ae33bed9944145636639a1b2
Glenn Maynard <glenn@zewt.org>
2021-04-18 22:00:55 -0500

    Update _pluginLoaded for API changes in Maya 2022.
    MSceneMessage.kAfterPluginLoad now has three arguments instead of two, and the third argument is
    what the second argument used to be.
    
    Fixes the "if not pluginName" short circuit below, so we don't waste time initializing
    plugins that we already know about.

--- pymel/core/__init__.py
+++ pymel/core/__init__.py
@@ -188,8 +188,13 @@ def _pluginLoaded(*args):
     global _pluginData
 
     if len(args) > 1:
-        # 2009 API callback, the args are ( [ pathToPlugin, pluginName ], clientData )
-        pluginName = args[0][1]
+        if len(args[0]) > 2:
+            # 2022 API callback, the args are ( [ pathToPlugin, pluginName1, pluginName2 ], clientData )
+            # pluginName2 is the old pluginName and is "" if the plugin was already loaded.
+            pluginName = args[0][2]
+        else:
+            # 2009 API callback, the args are ( [ pathToPlugin, pluginName ], clientData )
+            pluginName = args[0][1]
     else:
         pluginName = args[0]
     pluginName = _stripPluginExt(pluginName)
