6059060e0113da44aa1c8a57d7afd13d56267965
Chad Dombrova <chadrik@gmail.com>
2023-01-09 14:14:54 -0800

    Fix test script to honor mayapy2 selection for 2022

--- tests/pymel_test.bash
+++ tests/pymel_test.bash
@@ -3,9 +3,15 @@
 this_script=$(python -c "import os; import posixpath; print(os.path.normcase(os.path.normpath(os.path.realpath(os.path.abspath('''$0''')))).replace(os.sep, posixpath.sep))")
 echo "$this_script"
 
+if [[ "${MAYA_PYTHON_VERSION}" == 2 ]]; then
+    mayapy_name=mayapy2
+else
+    mayapy_name=mayapy
+fi
+
 print_python_module_dir() {
     module="$1"
-    mayapy -c '
+    $mayapy_name -c '
 from __future__ import print_function
 try:
     import '$module' as test_mod
@@ -22,6 +28,7 @@ else:
     print(mod_dir)
 '
 }
+
 this_dir=$(dirname "$this_script")
 echo "$this_dir"
 pymel_dir=$(dirname "$this_dir")
@@ -29,11 +36,6 @@ pymel_dir=$(dirname "$this_dir")
 pytest_dir=$(print_python_module_dir pytest)
 pkg_resources_dir=$(print_python_module_dir pkg_resources)
 
-if [[ "${MAYA_PYTHON_VERSION}" == 2 ]]; then
-    mayapy_name=mayapy2
-else
-    mayapy_name=mayapy
-fi
 mayapy_exc=$(which ${mayapy_name})
 mayapy_dir=$(dirname "${mayapy_exc}")
 
