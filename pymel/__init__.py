01c1e19cda064801749324c240b0182b59a7a490
Chad Dombrova <chadrik@gmail.com>
2024-06-01 12:48:28 -0700

    Bump version to 1.5.0

--- pymel/__init__.py
+++ pymel/__init__.py
@@ -28,7 +28,7 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 
-__versiontuple__ = (1, 4, 0)
-__version_suffix__ = 'b1'
+__versiontuple__ = (1, 5, 0)
+__version_suffix__ = ''
 __version__ = '.'.join(str(x) for x in __versiontuple__) + __version_suffix__
 __authors__ = ['Chad Dombrova', 'Paul Molodowitch', 'Olivier Renouard', 'Ofer Koren']
